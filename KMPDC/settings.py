11ad94676fa8cd28d000b5b331520fe3fa06d3e9
motanyafrancis <francismotanya@outlook.comsource>
2023-05-30 23:20:48 +0300

    Profile changes

--- KMPDC/settings.py
+++ KMPDC/settings.py
@@ -15,9 +15,14 @@ ALLOWED_HOSTS = ['.localhost', '.127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'kephis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
+
 }
 
 INSTALLED_APPS = [
